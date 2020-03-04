c9feeeb26aed088e7773be8858ae0bd1e0b55e5f
root <root@mad-everis.usersad.everis.int>
2020-03-04 17:31:01 -0300

    adicao de botões

--- adicionar.php
+++ adicionar.php
@@ -4,6 +4,14 @@
 	$quantidade = $_POST['quantidade'];
 	echo('Recebi '.$quantidade.' do produto '.$produto." foram inseridos<br><br>");
 	
+	$connect = mysqli_connect("127.0.0.1", "admin", "Xurumela@19", "Empresa");
+
+	if($connect){
+		$query = "INSERT INTO Estoque (produto,quantidade)VALUES('".$produto."','".$quantidade."')";
+		mysqli_query($connect,$query);
+		mysqli_close($connect);
+	}
+
 	echo('<a href="index.php">Clique aqui para retornar</a>');
 ?>
 
--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<?php
-	echo'<marquee>Site Generico</marquee>';
-?>
 <!DOCTYPE HTML>
 <HTML>
 	<BODY>
+		<H1>ESTOQUE</h1>
+		<H3>Inserir</h3>
 		<form method="POST" action="adicionar.php">
 			<label for='produto'>Produto:</label>
 			<input id="produto" name="produto" type='text'>
@@ -11,5 +10,45 @@
 			<input id="quantidade"  name="quantidade" type='text'>		
 			<input type="submit" value="Inserir"/>
 		</form>
+		
+			<?php
+				$connect = new mysqli("127.0.0.1", "admin", "Xurumela@19", "Empresa");
+			
+					if($connect){
+						$query = "Select id, produto, quantidade from Estoque";
+						$resultset = $connect->query($query);
+						
+						if($resultset->num_rows == 0){
+						
+						}else{
+							echo("<table>");
+								echo("<tr>");
+									echo("<th>Produto</th>");
+									echo("<th>Quantidade</th>");
+									echo("<th>Ações</th>");
+								echo("</tr>");
+							while($rs = $resultset->fetch_assoc()){
+								echo("<tr>");
+									echo("<td>");
+										echo($rs['produto']);	
+									echo("</td>");
+									echo("<td>");
+										echo($rs['quantidade']);	
+									echo("</td>");	
+									echo("<td>");
+										echo("<form method='POST' action='excluir.php'><input id='id' name='id' type='hidden' value='".$rs['id']."'>"."<input type='submit' value='Excluir'/></form>");
+										echo("<form method='POST' action='alterar.php'><input id='id' name='id' type='hidden' value='".$rs['id']."'>"."<input type='submit' value='Alterar'/></form>");								
+									echo("</td>");
+								echo("</tr>");
+							}
+						mysqli_close($connect);
+						echo("</table>");
+					}	
+					}
+			?>
+		
 	</BODY>
 </HTML>
+
+
+
